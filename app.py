2abf0d23d1abfd34ab0a2785b0a89be44292972c
CAPYBARA <nsypenj@pust2.zzso.ukr.education>
2024-11-30 12:31:37 +0200

    corn

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from db import DatabaseManager
 app = Flask(__name__)  # Створюємо веб–додаток Flask
 db = DatabaseManager("data.db")
 
-@app.route("/")  # Вказуємо url-адресу для виклику функції
+@app.route("/")
+# Вказуємо url-адресу для виклику функції
 def index():
     articles = db.get_all_articles()
-    
-    return render_template("index.html", articles=articles)  # html-сторінка, що повертається у браузер
+    return render_template("index.html", articles-articles)
 
+@app.route("/articles/<int:article_id>") # Вказуємо url-адресу для виклику функції
+def article_page(article_id):
+    article = db.get_article(article_id)
 
+    return render_template("article.html", article=article) # html-сторінка, що повертається у браузер# html-сторінка, що повертається у браузер
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
     app.run(debug=True)  # Запускаємо веб-сервер з цього файлу в режимі налагодження
